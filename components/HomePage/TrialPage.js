c471cd8cfd5e6c1054faf0c0f739ec7e2a5dbcfe
moraj <dhwajmoraj7@gmail.com>
2022-12-05 23:48:34 +0530

    Added bg Iamge.

---
+++ components/HomePage/TrialPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const TrialPage = () => {
+  return (
+    <div>
+      <div
+          className="h-full w-full bg-cover "
+        style={{
+          backgroundImage: `url(/codebg.png)`,
+          // width: "100",
+          // height: "100",
+        }}
+      >
+
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+      </div>
+    </div>
+  );
+};
+
+export default TrialPage;
