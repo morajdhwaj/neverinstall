583c666432201c757a13c654a73a60b98b2b6d0b
ankitpandey71 <ankitpandeyxy@gmail.com>
2022-12-06 10:12:56 +0530

    done changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import BoostPage from "../components/HomePage/BoostPage";
 import BrowserPage from "../components/HomePage/BrowserPage";
 import ChangewayPage from "../components/HomePage/ChangewayPage";
 import CodePage from "../components/HomePage/CodePage";
-import styles from "../styles/Home.module.css";
 import AccordionPage from "../components/HomePage/AccordionPage";
 import CollaborationPage from "../components/HomePage/CollaborationPage";
 import Productivity from "../components/HomePage/Productivity";
